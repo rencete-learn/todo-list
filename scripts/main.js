e3000b2489e18fbe50b5ccb992545f32c0025b13
Terence Yu <rencete@yahoo.com>
2018-05-11 22:19:38 +0800

    Deletion of tasks when clicking on span

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+// Check off completed tasks
 $("li").click(function() {
     $(this).toggleClass("completed")
+})
+
+// Handling of deletion of tasks
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove()
+    })
+    event.stopPropagation()
 })
